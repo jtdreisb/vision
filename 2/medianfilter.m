7f060a17619a2c210af55b90fda373e4712b85a0
Jason Dreisbach <jtdreisb@gmail.com>
2011-10-04 10:17:34 -0700

    added medianfilter and edge filter files

---
+++ 2/medianfilter.m
@@ -0,0 +1,20 @@
+% Median filter
+
+
+function [output] = mymedian(input, dimension) 
+
+[inputHeight, inputWidth] = size(input);
+offset = (dimension+1)/2;
+
+% new is the buffer that we store the values into after applying the filter
+new = zeros(inputHeight, inputWidth);
+
+for i=offset:(inputWidth-offset+1)
+   locx = i-offset+1;
+   for j=offset:inputHeight-offset+1
+      locy = j-offset+1;
+      mask = input(locy:locy+dimension-1, locx:locx+dimension-1);
+      new(j,i) = sum(mask(:))/div;
+   end
+end
+output = new
