60af7703e414a559cac6832a0713b23d9f9d6f2a
Jason Dreisbach <jtdreisb@gmail.com>
2011-10-11 09:24:40 -0700

    iterates through the faces directories

--- 3/project3.m
+++ 3/project3.m
@@ -1,14 +1,22 @@
 % Project 3
 
 figure;
+
 dirlist = dir('faces/*');
+basedir = 'faces';
 
-for x=1: length(dirlist)
+for x=3: length(dirlist)
 	if (dirlist(x).isdir==1)%make sure it's a dir
-	cd dirlist(x)
-	imlist=dir('*.jpg');
-
-	for y=1: length(imlist)
-		
-	end
+        imgdir = strcat(basedir,'/',dirlist(x).name)
+        imgsearch = strcat(imgdir,'/*.jpg');
+        
+        imlist=dir(imgsearch);
+       
+        for y=1: length(imlist)
+            imgpath = strcat(imgdir,'/',imlist(y).name);
+            imshow(imread(imgpath));
+            H = imrect;
+            pos = wait(H);
+        end
+    end
 end
