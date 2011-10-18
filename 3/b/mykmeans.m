9080289d73c33fd50a487fabfa9a2f19a7ef671e
Jason Dreisbach <jtdreisb@gmail.com>
2011-10-18 10:28:22 -0700

    update

--- 3/b/mykmeans.m
+++ 3/b/mykmeans.m
@@ -1,7 +1,20 @@
-%hello
+% k means
+% Project 3 b
+% Jason, Blake, Jen
 
 function [output] = mykmeans(input, k) 
-
+    [height, width, depth] = size(input);
+    
+    inputmag = input(:,:,1);
+    
+    for r=1 : height
+        for c=1 : width
+            inputmag = sqrt(input(r,c,1)*input(r,c,1)+input(r,c,2)*input(r,c,2)+input(r,c,3)*input(r,c,3));
+        end
+    end
+    
+    
+    output = input(:,:,1);
     
     % initialize the means
     means = [];
@@ -12,13 +25,11 @@ function [output] = mykmeans(input, k)
     done = 0;
     while done == 0
         % iterate through the image
-        [height, width, depth] = size(input);
-        
-        meanvalues = getmeanvalues(input,means);
+        meanvalues = inputmag(means);
         
         for i=1 : width
             for j=1 : height
-                
+                output(j,i) = closestmean(meanvalues,input(j,i));
             end
         end
         done = 1;
