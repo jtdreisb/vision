e68fe517c5b203fde07abb1dd5c35634ecee4a00
Jason Dreisbach <jtdreisb@gmail.com>
2011-10-18 09:47:05 -0700

    starter code for mean values

--- 3/b/getmeanvalues.m
+++ 3/b/getmeanvalues.m
@@ -1,1 +1,10 @@
-This is good
+% get mean values
+
+function [values] = getmeanvalues(input, means)
+    values = [];
+    meansize = size(means);
+    for i=1: meansize(1) % number of means
+        values = [values; input(means(i))];
+    end
+
+end
