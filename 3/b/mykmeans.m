e74539c01505188a9ad8933feffb3e3aeb181926
Jason Dreisbach <jtdreisb@gmail.com>
2011-10-18 09:47:39 -0700

    update of kmeans

--- 3/b/mykmeans.m
+++ 3/b/mykmeans.m
@@ -1,1 +1,27 @@
-placeholder
+%hello
+
+function [output] = mykmeans(input, k) 
+
+    
+    % initialize the means
+    means = [];
+    for i=1: k
+        means = [means;[i,1,1]];
+    end
+    
+    done = 0;
+    while done == 0
+        % iterate through the image
+        [height, width, depth] = size(input);
+        
+        meanvalues = getmeanvalues(input,means);
+        
+        for i=1 : width
+            for j=1 : height
+                
+            end
+        end
+        done = 1;
+    end
+    
+end
