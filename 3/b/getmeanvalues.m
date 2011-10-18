66b93d9d321ac201ec090d1b0d55aba5b4e616ba
Jason Dreisbach <jtdreisb@gmail.com>
2011-10-18 10:41:15 -0700

    mean values should work

--- 3/b/getmeanvalues.m
+++ 3/b/getmeanvalues.m
@@ -2,9 +2,9 @@
 
 function [values] = getmeanvalues(input, means)
     values = [];
+    % means is a Kx3 matrix  
     meansize = size(means);
     for i=1: meansize(1) % number of means
-        values = [values; input(means(i))];
+        values = [values; input(means(i,:))];
     end
-
 end
