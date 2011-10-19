a442a5281ef1d7c6a0e782bff561aa737d584240
Jason Dreisbach <jtdreisb@gmail.com>
2011-10-18 20:39:43 -0700

    Bug. The means do not update. But this is about 80% there.

--- 3/b/mykmeans.m
+++ 3/b/mykmeans.m
@@ -3,13 +3,14 @@
 % Jason, Blake, Jen
 
 function [output] = mykmeans(input, k) 
+    input = double(input);
     [height, width, depth] = size(input);
     
     inputmag = input(:,:,1);
     
     for r=1 : height
         for c=1 : width
-            inputmag = sqrt(input(r,c,1)*input(r,c,1)+input(r,c,2)*input(r,c,2)+input(r,c,3)*input(r,c,3));
+            inputmag(r,c) = norm(input(r,c));
         end
     end
     
@@ -19,20 +20,37 @@ function [output] = mykmeans(input, k)
     % initialize the means
     means = [];
     for i=1: k
-        means = [means;[i,1,1]];
+        means = [means;[i,1]];
     end
     
+    
     done = 0;
-    while done == 0
+    while done < 2
         % iterate through the image
         meanvalues = getmeanvalues(inputmag,means);
-        
+
         for i=1 : width
             for j=1 : height
                 output(j,i) = closestmean(meanvalues,inputmag(j,i));
             end
         end
-        done = 1;
+        
+        % iterate through the ouput array finding
+        % The calculating the new location of the means
+        newmeans = [];
+        for n=1: k
+            [rows, columns]= find(output == n);
+            if size(rows) > 0
+                newmeans = [newmeans;[round(mean(rows)), round(mean(columns))]];
+            else
+                newmeans = [newmeans; means(n,:)];
+            end
+        end
+        
+        % Check to see if we moved anything
+        done = (newmeans == means);
+        means = newmeans;
+
     end
     
 end
