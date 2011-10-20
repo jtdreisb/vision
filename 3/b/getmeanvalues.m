707445191314c781777a32b81d708880de74afa5
Jason Dreisbach <jtdreisb@gmail.com>
2011-10-19 19:47:37 -0700

    fixed bug indexing into the array

--- 3/b/getmeanvalues.m
+++ 3/b/getmeanvalues.m
@@ -7,6 +7,9 @@ function [values] = getmeanvalues(input, means)
     % means is a Kx2 matrix  
     meansize = size(means);
     for i=1: meansize(1) % number of means
-        values = [values; input(means(i,:))];
+        point = means(i,:);
+        values = [values; input(point(1),point(2))];
     end
+    % values is the vector described by the point that it corresponds to in
+    % means
 end
