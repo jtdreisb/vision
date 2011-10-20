d600cf1aa762b83121d9810778da91de97f76923
Jason Dreisbach <jtdreisb@gmail.com>
2011-10-20 11:39:23 -0700

    update doimg

--- 3/b/doimg.m
+++ 3/b/doimg.m
@@ -1,1 +1,13 @@
-placeholder
+% doimg(takes an image as an input and puts images into figures
+% project 3
+% Jason, Blake, Jen
+function [ ] = doimg( inputimg )
+    figure
+    subplot(331); imshow(inputimg); title('Input');
+    for i=2 : 9     
+        out = mykmeans(inputimg,i);
+        subplot(3,3,i);imshow(label2rgb(out));
+        title(sprintf('k = %d', i));
+    end
+end
+
