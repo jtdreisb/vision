5b09596b0628689dd9c18c82eca3be52952941ac
Jason Dreisbach <jtdreisb@gmail.com>
2011-10-19 21:02:52 -0700

    fix

--- 3/b/getmeanvalues.m
+++ 3/b/getmeanvalues.m
@@ -6,10 +6,12 @@ function [values] = getmeanvalues(input, means)
     values = [];
     % means is a Kx2 matrix  
     meansize = size(means);
+    
     for i=1: meansize(1) % number of means
         point = means(i,:);
-        values = [values; input(point(1),point(2))];
+        values = [values; input(point(1),point(2),:)];
     end
+    
     % values is the vector described by the point that it corresponds to in
     % means
 end
