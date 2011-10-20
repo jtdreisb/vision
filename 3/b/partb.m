ccf92faec3a1db9c02db35a9b84e829cce3d1e7f
Jason Dreisbach <jtdreisb@gmail.com>
2011-10-20 11:40:23 -0700

    update partb.m

--- 3/b/partb.m
+++ 3/b/partb.m
@@ -1,1 +1,11 @@
-Heyo
+% This will generate both all of the kmean resulta
+% project 3
+% Jason, Blake, Jen
+
+
+% this will take quite a while to complete
+land = imread('./landscape.jpg');
+group = imread('./group.jpg');
+
+doimg(land);
+doimg(group);
