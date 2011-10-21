3c1c576292c72a4b400675a245deef661688875e
Jason Dreisbach <jtdreisb@gmail.com>
2011-10-20 17:10:46 -0700

    changes to part b

--- 3/b/partb.m
+++ 3/b/partb.m
@@ -6,6 +6,11 @@
 % this will take quite a while to complete
 land = imread('./landscape.jpg');
 group = imread('./group.jpg');
+face = imread('./face.jpg');
 
 doimg(land);
 doimg(group);
+
+pt = facekmean(face, 3);
+figure; imshow(face);
+hold on; plot(pt(1), pt(2), 'Marker', 'p', 'Color', [.88 .48 0], 'MarkerSize', 20);
