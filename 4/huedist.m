a3a52e2653a3778b07ad75d31b75ad68a2ef1321
Btraft <blake.t.rafter@gmail.com>
2011-10-25 10:09:32 -0700

    update of huedists.m

--- 4/huedist.m
+++ 4/huedist.m
@@ -1,1 +1,11 @@
-placeholder
+% returns the hue distributions for face and nonface pixels
+% project 4
+% Jason, Blake, Jen
+
+function [facehue, otherhue] = huedist(inputimage,mask)
+
+            HSVimage = rgb2hsv(inputimage);
+            normhue = mod(HSVimage(:,:,1)+.2,1);
+            otherhue = normhue .* double(mask==0);
+            facehue = nonzeros(normhue .* double(mask>0));
+end
