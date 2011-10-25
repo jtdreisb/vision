7517f3e530651707053a4088a177ed2d03c51f44
Btraft <blake.t.rafter@gmail.com>
2011-10-25 10:07:17 -0700

    update project4.m

--- 4/project4.m
+++ 4/project4.m
@@ -1,5 +1,5 @@
 % Collects the face masks for all of the images and saves them to disk
-% project 3
+% project 4
 % Jason, Blake, Jen
 
 dirlist = dir('faces/*');
@@ -7,30 +7,31 @@ basedir = 'faces';
 
 maskdir = 'masks';
 mkdir masks;
-
-for x=3: length(dirlist)
+        % This is a hack to reduce the amount of computation required per
+        % iteration of the code. The perception of speed is inversely
+        % proportional to the number of pictures that we have to calculate
+        % the hue and distribution on. We still have to write the code to
+        % find the mean and multiple peaks of the distribution curve.
+        x = 7;
+%for x=3: length(dirlist)
 	if (dirlist(x).isdir==1)%make sure it's a dir
         imgdir = strcat(basedir,'/',dirlist(x).name);
         imgsearch = strcat(imgdir,'/*.jpg');
+        maskfolder = strcat(maskdir,'/',dirlist(x).name);
         
         imlist=dir(imgsearch);
-       
+        
         for y=1: length(imlist)
             imgpath = strcat(imgdir,'/',imlist(y).name);
-            maskimage = plotdists(imgpath);
-
-            maskpath = strcat(maskdir,'/',dirlist(x).name,'/',imlist(y).name);
-            imwrite(maskimage,maskpath,'jpg');
-
+            img = imread(imgpath);
+            maskpath = strcat(maskfolder,'/',imlist(y).name);
+            mask = imread(maskpath);
+            [facehue,otherhue] = huedist(img,mask);
+            
+            figure; 
+            subplot(211);imhist(facehue);title('Face');
+            subplot(212);imhist(otherhue); title('Other');
         end
     end
-end
-
-manfacefind('./faces/1/image_0001.jpg');
-
-mask = plotdists('./faces/landscape.jpg');
-imwrite(mask,'masks/landscape','jpg');
-
-mask = plotdists('./faces/group.jpg');
-imwrite(mask,'masks/group','jpg');
+%end
 
