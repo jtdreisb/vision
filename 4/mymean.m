dd6c9734e52835aa8cf5f07a3071b38d321c0e81
Btraft <blake.t.rafter@gmail.com>
2011-10-25 10:30:33 -0700

    Updated working mymean.m

--- 4/mymean.m
+++ 4/mymean.m
@@ -1,1 +1,10 @@
-calculate the mean
+% returns the mean of hue distributions
+% project 4
+% Jason, Blake, Jen
+
+function [meannum] = mymean(distribution)
+
+            distsum = sum(distribution);
+            length = size(distribution);
+            meannum = distsum/length(1);
+end
