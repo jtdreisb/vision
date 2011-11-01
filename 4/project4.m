0a53cdf890ea25673494987981897ee92586533a
Jason Dreisbach <jtdreisb@gmail.com>
2011-11-01 09:50:04 -0700

    ok

--- 4/project4.m
+++ 4/project4.m
@@ -2,85 +2,114 @@
 % project 4
 % Jason, Blake, Jen
 
-dirlist = dir('faces/*');
-basedir = 'faces';
-x=7;
-maskdir = 'masks';
-facehue  = [];
-otherhue = [];  
-facegreen = [];
-othergreen = [];
-%for x=3: length(dirlist)
-	if (dirlist(x).isdir==1)%make sure it's a dir
-        imgdir = strcat(basedir,'/',dirlist(x).name);
-        imgsearch = strcat(imgdir,'/*.jpg');
-        maskfolder = strcat(maskdir,'/',dirlist(x).name);
-        
-        imlist=dir(imgsearch);
-        
-        for y=1: length(imlist)
-            imgpath = strcat(imgdir,'/',imlist(y).name);
-            img = imread(imgpath);
-            maskpath = strcat(maskfolder,'/',imlist(y).name);
-            mask = imread(maskpath);
+% dirlist = dir('faces/*');
+% basedir = 'faces';
+% x=7;
+% maskdir = 'masks';
+% facehue  = [];
+% otherhue = [];  
+% facegreen = [];
+% othergreen = [];
+% %for x=3: length(dirlist)
+% 	if (dirlist(x).isdir==1)%make sure it's a dir
+%         imgdir = strcat(basedir,'/',dirlist(x).name);
+%         imgsearch = strcat(imgdir,'/*.jpg');
+%         maskfolder = strcat(maskdir,'/',dirlist(x).name);
+%         
+%         imlist=dir(imgsearch);
+%         
+%         for y=1: length(imlist)
+%             imgpath = strcat(imgdir,'/',imlist(y).name);
+%             img = imread(imgpath);
+%             maskpath = strcat(maskfolder,'/',imlist(y).name);
+%             mask = imread(maskpath);
+% 
+%             HSVimage = rgb2hsv(img);
+%             normhue = mod(HSVimage(:,:,1)+.2,1);
+% 
+%             [fhue,ohue] = facedist(normhue,mask);
+%             facehue = [facehue; fhue(:)];
+%             otherhue = [otherhue; ohue(:)];
+% 
+%             %repeat for normalized green
+%             img = double(img);
+%             
+%             normgreen = img(:,:,2) ./ (img(:,:,1) + img(:,:,2) + img(:,:,3) + 1);
+%             [fgreen, ogreen] = facedist(normgreen, mask);
+% 
+%             facegreen = [facegreen; fgreen(:)];
+%             othergreen = [othergreen; ogreen(:)];
+%         end
+% 
+%     end
+% %end
+% 
+% % load('distributions.mat');
+% figure; 
+% subplot(221);imhist(facehue);title('Face'); 
+% 
+% x = 0:0.01:1;
+% 
+% hfacemean = mymean(facehue(:));
+% hfacevar = myvariance(facehue(:),hfacemean);
+% 
+% y = gaussmf(x,[sqrt(hfacevar) hfacemean]);
+% subplot(223); plot(x,y);
+% 
+% 
+% subplot(222);imhist(otherhue); title('Other');
+% 
+% hothermean = mymean(otherhue(:));
+% hothervar = myvariance(otherhue(:),hothermean);
+% 
+% y = gaussmf(x,[sqrt(hothervar) hothermean]);
+% subplot(224); plot(x,y);
+% 
+% 
+% figure; 
+% subplot(221);imhist(facegreen);title('Face'); 
+% 
+% gfacemean = mymean(facegreen(:));
+% gfacevar = myvariance(facegreen(:),gfacemean);
+% 
+% y = gaussmf(x,[sqrt(gfacevar) hfacemean]);
+% subplot(223); plot(x,y);
+% 
+% subplot(222);imhist(othergreen); title('Other');
+% 
+% gothermean = mymean(facegreen(:));
+% gothervar = myvariance(facegreen(:),gfacemean);
+% 
+% y = gaussmf(x,[sqrt(gothervar) gothermean]);
+% subplot(224); plot(x,y);
+% 
+% save('gaussians.mat', 'hfacemean','hfacevar','hothermean','hothervar','gfacemean','gfacevar','gothermean', 'gothervar');
+load('gaussians.mat');
 
-            HSVimage = rgb2hsv(img);
-            normhue = mod(HSVimage(:,:,1)+.2,1);
+groupimg = imread('faces/group.jpg');
 
-            [fhue,ohue] = facedist(normhue,mask);
-            facehue = [facehue; fhue(:)];
-            otherhue = [otherhue; ohue(:)];
 
-            %repeat for normalized green
-            img = double(img);
-            
-            normgreen = img(:,:,2) ./ (img(:,:,1) + img(:,:,2) + img(:,:,3) + 1);
-            [fgreen, ogreen] = facedist(normgreen, mask);
+grouphsv = rgb2hsv(groupimg);
+grouphue = grouphsv(:,:,1);
+[groupheight, groupwidth] = size(grouphue);
 
-            facegreen = [facegreen; fgreen(:)];
-            othergreen = [othergreen; ogreen(:)];
+% skin classification using nearest mean classifier
+hnmo=zeros([groupheight groupwidth]);
+for r=1; groupheight
+    for c=1; groupwidth
+        if (abs(hfacemean-grouphue(r,c)) < abs(hothermean-grouphue(r,c)))
+            hnmo(r,c) = 1;
         end
-
     end
-%end
+end
 
-% load('distributions.mat');
-figure; 
-subplot(221);imhist(facehue);title('Face'); 
+% bayes classification
+hbco = zeros([groupheight, groupwidth]);
 
-x = 0:0.01:1;
+for r=1; groupheight
+    for c=1;groupwidth
+        %bayes
+    end
+end
 
-hfacemean = mymean(facehue(:));
-hfacevar = myvariance(facehue(:),hfacemean);
-
-y = gaussmf(x,[sqrt(hfacevar) hfacemean]);
-subplot(223); plot(x,y);
-
-
-subplot(222);imhist(otherhue); title('Other');
-
-hothermean = mymean(otherhue(:));
-hothervar = myvariance(otherhue(:),hothermean);
-
-y = gaussmf(x,[sqrt(hothervar) hothermean]);
-subplot(224); plot(x,y);
-
-
-figure; 
-subplot(221);imhist(facegreen);title('Face'); 
-
-gfacemean = mymean(facegreen(:));
-gfacevar = myvariance(facegreen(:),gfacemean);
-
-y = gaussmf(x,[sqrt(gfacevar) hfacemean]);
-subplot(223); plot(x,y);
-
-subplot(222);imhist(othergreen); title('Other');
-
-gothermean = mymean(facegreen(:));
-gothervar = myvariance(facegreen(:),gfacemean);
-
-y = gaussmf(x,[sqrt(gothervar) gothermean]);
-subplot(224); plot(x,y);
-
-save('gaussians.mat', 'hfacemean','hfacevar','hothermean','hothervar','gfacemean','gfacevar','gothermean', 'gothervar');
+ 
