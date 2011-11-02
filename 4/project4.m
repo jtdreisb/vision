5706f61265fed06049c0a7d727f987990b480913
Jason Dreisbach <jtdreisb@gmail.com>
2011-11-01 19:40:37 -0700

    new function to do the classifiers

--- 4/project4.m
+++ 4/project4.m
@@ -44,84 +44,57 @@ othergreen = [];
     end
 %end
 
-% load('distributions.mat');
+% find the hue distributions 
 figure; 
 subplot(221);imhist(facehue);title('Face'); 
 
 x = 0:0.01:1;
 
-hfacemean = mymean(facehue(:));
-hfacevar = myvariance(facehue(:),hfacemean);
+h_m_skin = mymean(facehue(:));
+h_s_skin = sqrt(myvariance(facehue(:),hfacemean));
 
-y = gaussmf(x,[sqrt(hfacevar) hfacemean]);
-subplot(223); plot(x,y);
+y = gaussmf(x,[h_s_skin h_m_skin]);
+subplot(223); plot(x,y); title('Skin distribution');
 
 
 subplot(222);imhist(otherhue); title('Other');
 
-hothermean = mymean(otherhue(:));
-hothervar = myvariance(otherhue(:),hothermean);
+h_m_other = mymean(otherhue(:));
+h_s_other = sqrt(myvariance(otherhue(:),h_m_other));
 
-y = gaussmf(x,[sqrt(hothervar) hothermean]);
-subplot(224); plot(x,y);
+y = gaussmf(x,[h_s_other h_m_other]);
+subplot(224); plot(x,y); title('Other distribution');
 
 
+% find normalized green components
 figure; 
 subplot(221);imhist(facegreen);title('Face'); 
 
-gfacemean = mymean(facegreen(:));
-gfacevar = myvariance(facegreen(:),gfacemean);
+g_m_skin = mymean(facegreen(:));
+g_s_skin = sqrt(myvariance(facegreen(:), g_m_skin));
 
-y = gaussmf(x,[sqrt(gfacevar) hfacemean]);
-subplot(223); plot(x,y);
+y = gaussmf(x,[g_s_skin g_m_skin]);
+subplot(223); plot(x,y); title('Skin distribution');
 
 subplot(222);imhist(othergreen); title('Other');
 
-gothermean = mymean(facegreen(:));
-gothervar = myvariance(facegreen(:),gfacemean);
+g_m_other = mymean(facegreen(:));
+g_s_other = sqrt(myvariance(facegreen(:),g_m_other));
 
-y = gaussmf(x,[sqrt(gothervar) gothermean]);
-subplot(224); plot(x,y);
+y = gaussmf(x,[g_s_other g_m_other]);
+subplot(224); plot(x,y); title('Other distribution');
 
-% save('gaussians.mat', 'hfacemean','hfacevar','hothermean','hothervar','gfacemean','gfacevar','gothermean', 'gothervar');
-% load('gaussians.mat');
+
+count_skin = length(facehue);
+count_other = length(otherhue);
+p_skin = h_count_skin/(h_count_skin+h_count_other);
+p_other = h_count_other/(h_count_skin+h_count_other);
+
 
 groupimg = imread('faces/group.jpg');
 
-grouphsv = rgb2hsv(groupimg);
-grouphue = mod(grouphsv(:,:,1)+.2,1);
-[groupheight, groupwidth] = size(grouphue);
+findskin(groupimg, p_skin, p_other, h_m_skin, h_m_other, h_s_skin, h_s_other);
 
-% skin classification using nearest mean classifier
-hnmo=zeros([groupheight groupwidth]);
-for r=1: groupheight;
-    for c=1: groupwidth;
-        % if it is closer to the mean it is a face pixel
-        if (abs(hfacemean-grouphue(r,c)) < abs(hothermean-grouphue(r,c)))
-            hnmo(r,c) = 1;
-        end
-    end
-end
 
-figure;
-imshow(hnmo); title('Nearest mean classifier on hue');
 
-% bayes classification
-hbco = zeros([groupheight, groupwidth]);
 
-hfacepxcount = size(facehue);
-hotherpxcount = size(otherhue);
-hpface = hfacepxcount(1)/(hotherpxcount(1)+hfacepxcount(1));
-hpother = hotherpxcount(1)/(hotherpxcount(1)+hfacepxcount(1));
-
-for r=1: groupheight;
-    for c=1:groupwidth;
-        hbayesface = hpface * gauss_distribution(grouphue(r,c),hfacemean, sqrt(hfacevar));
-        hbayesother = hpother * gauss_distribution(grouphue(r,c),hothermean, sqrt(hothervar));
-        if (hbayesface > hbayesother)
-            hbco(r,c) = 1;
-        end
-    end
-end
-figure;
-imshow(hbco); title('Bayes Classifier on hues');
