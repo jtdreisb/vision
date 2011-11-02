ca185c126a0170766c84b60562dbcca283937c55
Jason Dreisbach <jtdreisb@gmail.com>
2011-11-01 20:18:22 -0700

    update

--- 4/project4.m
+++ 4/project4.m
@@ -10,7 +10,7 @@ facehue  = [];
 otherhue = [];  
 facegreen = [];
 othergreen = [];
-%for x=3: length(dirlist)
+% for x=3: length(dirlist)
     if (dirlist(x).isdir==1)%make sure it's a dir
         imgdir = strcat(basedir,'/',dirlist(x).name);
         imgsearch = strcat(imgdir,'/*.jpg');
@@ -42,7 +42,7 @@ othergreen = [];
         end
 
     end
-%end
+% end
 
 % find the hue distributions 
 figure; 
@@ -51,7 +51,7 @@ subplot(221);imhist(facehue);title('Face');
 x = 0:0.01:1;
 
 h_m_skin = mymean(facehue(:));
-h_s_skin = sqrt(myvariance(facehue(:),hfacemean));
+h_s_skin = sqrt(myvariance(facehue(:),h_m_skin));
 
 y = gaussmf(x,[h_s_skin h_m_skin]);
 subplot(223); plot(x,y); title('Skin distribution');
@@ -87,14 +87,33 @@ subplot(224); plot(x,y); title('Other distribution');
 
 count_skin = length(facehue);
 count_other = length(otherhue);
-p_skin = h_count_skin/(h_count_skin+h_count_other);
-p_other = h_count_other/(h_count_skin+h_count_other);
+p_skin = count_skin/(count_skin+count_other);
+p_other = count_other/(count_skin+count_other);
 
 
-groupimg = imread('faces/group.jpg');
-
-findskin(groupimg, p_skin, p_other, h_m_skin, h_m_other, h_s_skin, h_s_other);
-
+group_img = imread('faces/group.jpg');
 
 
 
+group_hsv = rgb2hsv(group_img);
+group_hue = mod(group_hsv(:,:,1)+.2,1);
+
+[nmout, bcout] = findskin(group_hue, p_skin, p_other, h_m_skin, h_m_other, h_s_skin, h_s_other);
+%display the data
+figure;
+subplot(311); imshow(group_img); title('Group hue');
+subplot(312); imshow(nmout); title('Nearest mean classifier');
+subplot(313); imshow(bcout); title('Bayesian classifier');
+group_img = double(group_img);
+group_green = group_img(:,:,2) ./ (group_img(:,:,1) + group_img(:,:,2) + group_img(:,:,3) + 1);
+[nmout,bcout]=findskin(group_green, p_skin, p_other, g_m_skin, g_m_other, g_s_skin, g_s_other);
+figure;
+subplot(311); imhist(group_green); title('Group green');
+subplot(312); imshow(nmout); title('Nearest mean classifier');
+subplot(313); imshow(bcout); title('Bayesian classifier');
+
+% face1 =  imread('faces/2/image_0022.jpg');
+% findskin(face1, p_skin, p_other, h_m_skin, h_m_other, h_s_skin, h_s_other);
+% face2 =  imread('faces/6/image_0113.jpg');
+% findskin(face2, p_skin, p_other, h_m_skin, h_m_other, h_s_skin, h_s_other);
+% % 
