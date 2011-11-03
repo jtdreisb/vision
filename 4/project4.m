c39c169be50822c5e926cae8dc5499280a4f2471
Jason Dreisbach <jtdreisb@gmail.com>
2011-11-03 11:24:06 -0700

    fixed bug with normalized green mean and variance calcs

--- 4/project4.m
+++ 4/project4.m
@@ -34,7 +34,8 @@ othergreen = [];
             %repeat for normalized green
             img = double(img);
             
-            normgreen = img(:,:,2) ./ (img(:,:,1) + img(:,:,2) + img(:,:,3) + 1);
+            normgreen = img(:,:,2) ./ (img(:,:,1) + img(:,:,2) + img(:,:,3));
+            normgreen(isnan(normgreen)) = 0;
             [fgreen, ogreen] = facedist(normgreen, mask);
 
             facegreen = [facegreen; fgreen(:)];
@@ -78,8 +79,8 @@ subplot(223); plot(x,y); title('Skin distribution');
 
 subplot(222);imhist(othergreen); title('Other');
 
-g_m_other = mymean(facegreen(:));
-g_s_other = sqrt(myvariance(facegreen(:),g_m_other));
+g_m_other = mymean(othergreen(:));
+g_s_other = sqrt(myvariance(othergreen(:),g_m_other));
 
 y = gaussmf(x,[g_s_other g_m_other]);
 subplot(224); plot(x,y); title('Other distribution');
@@ -89,11 +90,12 @@ count_skin = length(facehue);
 count_other = length(otherhue);
 p_skin = count_skin/(count_skin+count_other);
 p_other = count_other/(count_skin+count_other);
-
+if p_skin < .25
+    p_skin=.5;
+    p_other=.5;
+end
 
 group_img = imread('faces/group.jpg');
-
-
 
 group_hsv = rgb2hsv(group_img);
 group_hue = mod(group_hsv(:,:,1)+.2,1);
@@ -106,6 +108,7 @@ subplot(312); imshow(nmout); title('Nearest mean classifier');
 subplot(313); imshow(bcout); title('Bayesian classifier');
 group_img = double(group_img);
 group_green = group_img(:,:,2) ./ (group_img(:,:,1) + group_img(:,:,2) + group_img(:,:,3) + 1);
+group_green(isnan(group_green)) = 0;
 [nmout,bcout]=findskin(group_green, p_skin, p_other, g_m_skin, g_m_other, g_s_skin, g_s_other);
 figure;
 subplot(311); imhist(group_green); title('Group green');
